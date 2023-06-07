2be5f57865677f9028328acfee643bc9acb71421
AdityaX24 <poddar.aditya2014@gmail.com>
2023-06-07 14:22:59 +0530

    Updated Max Heap using array

--- C/Heaps/Heap_Using_Array_Bottom_Up.c
+++ C/Heaps/Heap_Using_Array_Bottom_Up.c
@@ -1,3 +1,7 @@
+//Code by Aditya Poddar
+//SRN PES2UG21CS036
+//for any help, contact: poddar.aditya2014@gmail.com
+
 #include<stdio.h>
 #include<stdlib.h>
 #define MAX 100     //maximun elements in the heap. Can be changed according to user need
